ab01696359e98835bc29cae399f7486a466967c9
Sam Schneider <145950227+samschneiderr@users.noreply.github.com>
2026-03-02 01:12:38 -0500

    Update test_car.py

--- tests/test_car.py
+++ tests/test_car.py
@@ -1,7 +1,6 @@
 import unittest
 from services.car_service import CarService
-from database import save_data
-
+from database import load_data, save_data
 
 class TestCarService(unittest.TestCase):
 
@@ -11,12 +10,9 @@ class TestCarService(unittest.TestCase):
 
     def test_add_car(self):
         self.car_service.add_car("Toyota", "Corolla", 50, "NY")
-        from database import load_data
         data = load_data()
-
         self.assertEqual(len(data["cars"]), 1)
         self.assertEqual(data["cars"][0]["brand"], "Toyota")
-
 
 if __name__ == "__main__":
     unittest.main()
