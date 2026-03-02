246b5ebe9bb132aba7067f5a6893f98a95b1aca8
Sam Schneider <145950227+samschneiderr@users.noreply.github.com>
2026-03-02 01:12:55 -0500

    Update test_booking.py

--- tests/test_booking.py
+++ tests/test_booking.py
@@ -2,23 +2,19 @@ import unittest
 from datetime import date
 from services.booking_service import BookingService
 from services.car_service import CarService
-from database import save_data, load_data
-
+from database import load_data, save_data
 
 class TestBookingService(unittest.TestCase):
 
     def setUp(self):
         save_data({"users": [], "cars": [], "bookings": []})
-
         self.car_service = CarService()
         self.car_service.add_car("Honda", "Civic", 60, "LA")
-
         self.booking_service = BookingService()
 
     def test_create_booking(self):
         start = date(2026, 3, 1)
         end = date(2026, 3, 5)
-
         result = self.booking_service.create_booking(
             username="john",
             car_id=1,
@@ -26,13 +22,10 @@ class TestBookingService(unittest.TestCase):
             end_date=end,
             add_ons=[]
         )
-
         self.assertTrue(result)
-
         data = load_data()
         self.assertEqual(len(data["bookings"]), 1)
         self.assertEqual(data["cars"][0]["status"], "Rented")
-
 
 if __name__ == "__main__":
     unittest.main()
