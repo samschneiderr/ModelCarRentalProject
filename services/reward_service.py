99a1c1e0e1f08bd7a00c935d9b4aad7fb60a910d
Sam Schneider <145950227+samschneiderr@users.noreply.github.com>
2026-03-02 01:11:48 -0500

    Update reward_service.py

--- services/reward_service.py
+++ services/reward_service.py
@@ -1,12 +1,2 @@
 class RewardService:
-
-    @staticmethod
-    def add_points(user, amount):
-        user["points"] = user.get("points", 0) + int(amount / 10)
-
-    @staticmethod
-    def redeem_points(user, points):
-        if user.get("points", 0) >= points:
-            user["points"] -= points
-            return True
-        return False
+    pass
