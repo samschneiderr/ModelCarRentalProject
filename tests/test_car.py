b46405ea415df85aa9a7f1da7f36a53a02cf9f2e
Sam Schneider <145950227+samschneiderr@users.noreply.github.com>
2026-03-02 00:18:03 -0500

    Update test_car.py

--- tests/test_car.py
+++ tests/test_car.py
@@ -1,1 +1,22 @@
+import unittest
+from services.car_service import CarService
+from database import save_data
 
+
+class TestCarService(unittest.TestCase):
+
+    def setUp(self):
+        save_data({"users": [], "cars": [], "bookings": []})
+        self.car_service = CarService()
+
+    def test_add_car(self):
+        self.car_service.add_car("Toyota", "Corolla", 50, "NY")
+        from database import load_data
+        data = load_data()
+
+        self.assertEqual(len(data["cars"]), 1)
+        self.assertEqual(data["cars"][0]["brand"], "Toyota")
+
+
+if __name__ == "__main__":
+    unittest.main()
