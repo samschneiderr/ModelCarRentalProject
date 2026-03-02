9e292b19207212dc6857ee4f847fecee24163581
Sam Schneider <145950227+samschneiderr@users.noreply.github.com>
2026-03-02 01:12:14 -0500

    Update test_auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -2,23 +2,15 @@ import unittest
 from services.auth_service import AuthService
 from database import load_data, save_data
 
-
 class TestAuthService(unittest.TestCase):
 
     def setUp(self):
-        # Reset database before each test
         save_data({"users": [], "cars": [], "bookings": []})
         self.auth = AuthService()
 
     def test_register_user(self):
-        result = self.auth.register(
-            username="john",
-            password="1234",
-            role="Customer",
-            email="john@test.com"
-        )
+        result = self.auth.register("john", "1234", "Customer", "john@test.com")
         self.assertTrue(result)
-
         data = load_data()
         self.assertEqual(len(data["users"]), 1)
 
@@ -30,7 +22,6 @@ class TestAuthService(unittest.TestCase):
     def test_login_fail(self):
         user = self.auth.login("fake", "wrong")
         self.assertIsNone(user)
-
 
 if __name__ == "__main__":
     unittest.main()
