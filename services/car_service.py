c63a3078d36c27b7b104cd057b7129b35380caa2
Sam Schneider <145950227+samschneiderr@users.noreply.github.com>
2026-03-02 00:45:36 -0500

    Update car_service.py

--- services/car_service.py
+++ services/car_service.py
@@ -1,1 +1,8 @@
+class car_service:
+    def __init__(self):
+        # initialization
+        pass
 
+    def add_car(self, brand, model, price, location):
+        # logic to add car
+        pass
